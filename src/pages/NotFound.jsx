fa132474a0603548dd140a2886b312bb76d6ab3f
Wasil Fawad <107706052+FasTrak1x@users.noreply.github.com>
2025-09-24 14:43:25 +0500

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -7,7 +7,7 @@ export default function NotFound() {
     <div className="min-h-[80vh] grid place-items-center">
       <div className="text-center">
         <h1 className="text-6xl font-black mb-3">404</h1>
-        <p className="text-[#c6cbcf] mb-6">Oops! Page not found.</p>
+        <p className="text-[#c6cbcf] mb-6">Haven't gotten around to coding this yet...</p>
         <Link
           to="/"
           className="inline-block px-5 py-3 border-2 rounded-md 
